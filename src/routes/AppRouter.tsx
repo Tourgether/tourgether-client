bb3441aef7e1f7838467a4965ae3e2f73c2f7ecb
wjdtjdgns <person3275@gmail.com>
2025-04-27 17:56:26 +0900

    feat: MainLayout 구현

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,21 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
-import Map from "../pages/Map";
-import MyPage from "../pages/MyPage";
-import SearchPage from "../pages/SearchPage";
-import MainTab from "../components/common/MainTab";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainLayout from '../components/common/MainLayout'; // ✅ 추가
+import Home from '../pages/Home';
+import Map from '../pages/Map';
+import MyPage from '../pages/MyPage';
+import SearchPage from '../pages/SearchPage';
 
 export default function AppRouter() {
   return (
     <Router>
       <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/map" element={<Map />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="*" element={<Home />} />
+        <Route element={<MainLayout />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="*" element={<Home />} />
+        </Route>
       </Routes>
-      <MainTab />
     </Router>
   );
 }
