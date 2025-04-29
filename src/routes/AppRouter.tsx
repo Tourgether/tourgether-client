3c81bec3e15617560a7486370022f496c9868d41
ljg0610 <jggy0627@naver.com>
2025-04-29 19:47:06 +0900

    feat: mypage-visit (#13)
    * feat: mypage ui 추가
    
    * feat: mypage css 수정 및 방문내역 페이지 추가

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,22 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MainLayout from '../components/common/MainLayout';
+import MainLayout from "../components/common/MainLayout";
 import Home from "../pages/Home";
 import Map from "../pages/Map";
 import MyPage from "../pages/MyPage";
 import SearchPage from "../pages/SearchPage";
 import AttractionDetailPage from "../pages/AttractionDetailPage";
+import VisitHistoryPage from "../pages/VisitHistoryPage";
 
 export default function AppRouter() {
   return (
     <Router>
       <Routes>
         <Route element={<MainLayout />}>
-            <Route path="/home" element={<Home />} />
-            <Route path="/map" element={<Map />} />
-            <Route path="/mypage" element={<MyPage />} />
-            <Route path="/search" element={<SearchPage />} />
-            <Route path="/attraction/:id" element={<AttractionDetailPage />} />
-        <Route path="*" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/attraction/:id" element={<AttractionDetailPage />} />
+          <Route path="/mypage/visits" element={<VisitHistoryPage />} />
+          <Route path="*" element={<Home />} />
         </Route>
       </Routes>
     </Router>
